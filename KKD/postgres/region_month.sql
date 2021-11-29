f0078fcb0b0ae2cc3af86ca73451dd046ade50b3
Sergey Spiridonov <spiridonovmpei@gmail.com>
2021-11-29 13:08:07 +0300

    дополнил сверки

--- KKD/postgres/region_month.sql
+++ KKD/postgres/region_month.sql
@@ -1,6 +1,6 @@
 select
 	target_month as MONTH,
-	/*STRING_AGG(region_pk_fk, ',') as region_pk_fk,*/
+	STRING_AGG(region_pk_fk, ',') as region_pk_fk,
 	SUM(vol_power_peak_unreg) AS vol_power_peak_unreg,
 	SUM(vol_power_peak_full) AS vol_power_peak_full,
 	count(*) as count_rows
